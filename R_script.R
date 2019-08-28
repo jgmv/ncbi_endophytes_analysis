20733c1c1e5139931a144525feacb19f47fd99fa
jgmv <jg.macia@gmail.com>
2019-08-28 15:30:03 +0200

    First commit

---
+++ R_script.R
@@ -0,0 +1,62 @@
+source("R_code/functions.R")
+
+### data preparation
+# data input
+endo <- read.csv("files/20190726_endophytic_Fungi.csv", h = T,
+  sep = "|")
+
+# standardize country names
+endo <- get_country(endo)
+
+# assign corrected taxonomy (splitdata set to reduce memory use)
+tax <- read.csv("files/taxonomy.csv", h = T, sep = ";")
+x <- 0:nrow(tax) %/% 1000
+temp <- split(tax, x)
+for(i in 1:length(temp)) {
+  temp[[i]] <- modify_taxonomy(temp[[i]])
+  message(paste("Dataset #", i, " done."))
+}
+tax <- do.call("rbind", temp)
+endo$kingdom <- tax$kingdom
+endo$class <- tax$class
+endo$order <- tax$order
+endo$family <- tax$family
+endo$genus <- tax$genus
+endo$species <- tax$species
+rm(tax, temp)
+
+# remove non-endophyte records
+endo <- remove_nonendophytes(endo)
+
+# find out organ of origin of endophytes
+endo <- get_plant_part(endo)
+
+# get host data
+endo <- correct_host_names(endo)
+endo <- get_host_data(endo)
+
+# save data
+saveRDS(endo, file = "output/endophytes_data.RData")
+endo <- readRDS("output/endophytes_data.RData")
+
+### data analysis
+# plot distribution of endophytes records
+plot_map(endo, trans = "log10") # output/endophytes_map.png
+
+# plant organs of origin
+tab_organ <- table(endo$plant_part)
+plot_isolation_organ(tab_organ)# output/isolation_organ.png
+
+# environmental vs. cultures
+tab_seq_source <- table(endo$environmental_sample)
+plot_seq_source(tab_seq_source) # output/seq_source.png
+
+# taxonomic distribution
+plot_tax(endo) # output/endoph_taxa.png, output/endoph_taxa_part.png
+
+# main hosts of endophytes
+tab_host <- table(endo$host_family)
+plot_host_plant(endo) # output/seq_source.png
+
+
+### end
---
+++ fetch_data.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+
+#myfile=$(date +"%F_all_Fungi")
+myfile=$(date +"%F_endophytic_fungi")
+
+
+### download necessary scripts
+# NCBI analysis
+if test -f ./scripts/search_ncbi_gi_by_term.py
+then
+  echo "./scripts/search_ncbi_gi_by_term.py found"
+else
+  git clone https://github.com/jgmv/ncbi_data_analysis.git scripts
+fi
+
+# bash scripts
+if test -f ./scripts/removeTaxonTag.sh
+then
+  echo "./scripts/removeTaxonTag.sh found"
+else
+  git clone https://github.com/jgmv/bash_seq_analysis.git scripts
+fi
+
+# system-wide access to scripts
+. scripts/*.sh
+export PATH="$PATH:scripts"
+
+
+### fetch data
+# retrieve GIs for all fungal ITS from GenBank
+#python scripts/search_ncbi_gi_by_term.py -o $myfile.gi 'Fungi[Organism] and (internal transcribed spacer OR ITS OR ITS1 OR ITS2) not chromosome not Neocallimastigales'
+
+# retrieve GIs for all fungal endophytes ITS from GenBank
+python scripts/search_ncbi_gi_by_term.py -o $myfile.gi 'Fungi[Organism] and (internal transcribed spacer OR ITS OR ITS1 OR ITS2) not chromosome and 100:2000[Sequence Length] and endoph*[ALL]'
+
+# get metadata from GIs (takes time, better use tool in https://www.ncbi.nlm.nih.gov/sites/batchentrez)
+python fetch_gb.py -o $myfile.gb $myfile.gi
+
+# convert GenBank data to table
+python get_metadata_from_gb.py -o $myfile.csv $myfile.gb
+
+# get fasta sequences from gb file
+python get_fasta_from_gb.py -o $myfile.fasta $myfile.gb
+
+mkdir -p files
+mv $myfile* files
+
+
+### identify ITS sequences
+mothur "#classify.seqs(fasta=files/20190726_endophytic_Fungi.fasta,\
+  template=scripts/data/UNITEv6_sh_dynamic_s.fasta,\
+  taxonomy=scripts/data/UNITEv6_sh_dynamic_s.tax, cutoff=60, probs=T)" 
+removeTaxonTag files/20190726_endophytic_Fungi.UNITEv6_sh_dynamic_s.wang.taxonomy \
+  files/taxonomy_boot.csv
+cp files/taxonomy_boot.csv files/taxonomy.csv
+sed -i 's/([^()]*)//g' files/taxonomy.csv
+
+
+### end
